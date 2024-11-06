a435ccfc052c3c3ef4e289e85b80d2f6436c2cfb
zannat20040 <shanzidadisha1234@gmail.com>
2024-11-06 17:02:03 +0600

    layout & flexbox of React Ntive(RN)

--- Day01/BasicReactNative/App.js
+++ Day01/BasicReactNative/App.js
@@ -1,18 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button title='rative button' />
-      <Text style={styles.button}>custom button</Text>
+    // ---------------- BASIC IS COMPLETE-----------
+
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    //   <Button title='rative button' />
+    //   <Text style={styles.button}>custom button</Text>
+    // </View>
+
+    // --------- START LEARN LAYOUT & FLEXBOX -------
+
+    <View style={styles.container2}>
+      <StatusBar style='auto' />
+      <TextInput placeholder='List your learning' style={styles.input}/>
+      <Button title={'Add'}/>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
+  container2:{
+    padding:20,
+    marginTop:10,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    gap:5
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -30,5 +48,13 @@ const styles = StyleSheet.create({
     borderWidth:2,
     borderRadius:5,
     marginTop:10
+  },
+  input:{
+    borderWidth:1 ,
+    borderRadius:5,
+    padding:3,
+    paddingLeft:10,
+    borderColor:'gray',
+    width:'85%'
   }
 });
