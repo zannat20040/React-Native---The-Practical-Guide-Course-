e9817d87bad00c02a8f898827d86d84293a1eeeb
zannat20040 <shanzidadisha1234@gmail.com>
2024-11-06 16:40:41 +0600

    Styling in React Ntive(RN)

--- Day01/BasicReactNative/App.js
+++ Day01/BasicReactNative/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title='rative button' />
+      <Text style={styles.button}>custom button</Text>
     </View>
   );
 }
@@ -17,4 +19,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    color:'purple',
+    fontSize:20,
+    padding:10
+  },
+  button:{
+    padding:16,
+    borderColor:'green',
+    borderWidth:2,
+    borderRadius:5,
+    marginTop:10
+  }
 });
