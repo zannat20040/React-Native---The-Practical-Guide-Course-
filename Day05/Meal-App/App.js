36c1729ee62bc9770685b9436bc369ba374ea5d8
zannat20040 <shanzidadisha1234@gmail.com>
2024-11-24 19:16:02 +0600

    navigation added

--- Day05/Meal-App/App.js
+++ Day05/Meal-App/App.js
@@ -2,26 +2,29 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import CountryScreen from "./screens/CountryScreen";
 
-import { NavigationContainer } from '@react-navigation/native'; // Navigation container
-import { createStackNavigator } from '@react-navigation/stack'; // Stack navigator
+import { NavigationContainer } from "@react-navigation/native"; // Navigation container
+import { createStackNavigator } from "@react-navigation/stack"; // Stack navigator
 
 // Example screen components
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={CountryScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer >
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={CountryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      x
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#fff',
     alignItems: "center",
     justifyContent: "center",
   },
