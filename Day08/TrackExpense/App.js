0c1ec091f5ab60981f2a646131a721a8d09ea356
zannat20040 <shanzidadisha1234@gmail.com>
2024-11-29 23:36:47 +0600

    day 8 start with a expense manage app

---
+++ Day08/TrackExpense/App.js
@@ -0,0 +1,21 @@
+// import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import BudgetSet from "./screens/BudgetSet";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name={"setbudget"} component={BudgetSet} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
---
+++ Day08/TrackExpense/screens/ShowAllExpense.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+export default function ShowAllExpense() {
+  return (
+    <View>
+      <Text>ShowAllExpense</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
