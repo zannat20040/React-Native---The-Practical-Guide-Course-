89f55c01860ee8cb01aaa03ba33425d63e519a58
zannat20040 <shanzidadisha1234@gmail.com>
2024-11-29 23:46:20 +0600

    Navigation added

--- Day08/TrackExpense/App.js
+++ Day08/TrackExpense/App.js
@@ -3,19 +3,22 @@ import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BudgetSet from "./screens/BudgetSet";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Stack = createNativeStackNavigator();
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name={"setbudget"} component={BudgetSet} />
-        </Stack.Navigator>
+      <Tabs.Navigator>
+        <Tabs.Screen
+          name="setbudget"
+          component={BudgetSet}
+          options={{ title: "Your Monthly Budget" }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
- 
-});
+const styles = StyleSheet.create({});
