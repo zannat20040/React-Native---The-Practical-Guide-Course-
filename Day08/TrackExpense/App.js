0ff25d058bd3b56e70ad8c2cf1e5b43559c1d6d3
zannat20040 <shanzidadisha1234@gmail.com>
2024-12-01 16:57:19 +0600

    tab added

--- Day08/TrackExpense/App.js
+++ Day08/TrackExpense/App.js
@@ -1,36 +1,65 @@
-// import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BudgetSet from "./screens/BudgetSet";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ExpenseAdd from "./screens/ExpenseAdd";
 import ShowAllExpense from "./screens/ShowAllExpense";
+import { Colors } from "./globalStyle/globalStyle";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Feather from '@expo/vector-icons/Feather';
 
 const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tabs.Navigator>
+      <Tabs.Navigator
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveBackgroundColor: Colors.light,
+          tabBarInactiveBackgroundColor: Colors.soft,
+          tabBarActiveTintColor: Colors.primary,
+          tabBarInactiveTintColor: Colors.secondary,
+          tabBarStyle: {
+            height: 60, 
+           
+          },
+         
+        }}
+      >
         <Tabs.Screen
           name="setbudget"
           component={BudgetSet}
-          options={{ title: "Your Monthly Budget" }}
+          options={{
+            title: "Your Monthly Budget",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="attach-money" size={size} color={color} style={{paddingTop:10}} />
+            ),
+          }}
         />
         <Tabs.Screen
           name="addexpense"
           component={ExpenseAdd}
-          options={{ title: "Add New Expense" }}
+          options={{
+            title: "Add New Expense",
+            tabBarIcon: ({ color, size }) => (
+              <Feather name="plus" size={size} color={color} style={{paddingTop:10}}/>
+            ),
+          }}
         />
         <Tabs.Screen
           name="showexpenses"
           component={ShowAllExpense}
-          options={{ title: "See Your Expenses" }}
+          options={{
+            title: "See Your Expenses",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="list" size={size} color={color} style={{paddingTop:10}}/>
+            )
+          }}
         />
       </Tabs.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({});
---
+++ Day08/TrackExpense/globalStyle/globalStyle.js
@@ -0,0 +1,6 @@
+export const Colors = {
+  primary: "#48CFCB",
+  secondary: "#229799",
+  soft: "#424242",
+  lightsoft: "#F5F5F5",
+};
